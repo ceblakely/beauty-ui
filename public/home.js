548d03c9139ff3b0e77f84913016689d4b991bc6
ceblakely <cblakelydev@gmail.com>
2021-12-23 22:02:57 -0600

    Started JS on homepage

---
+++ public/home.js
@@ -0,0 +1,11 @@
+const vogueImgQuote =
+  "In an age of online access and unending choice, customer engagement is something that Fenty has gotten right.";
+const elleImgQuote =
+  "Revolutionary for its time, harnessing a certain come-as-you-are acceptance.";
+const vanityFairImgQuote =
+  "Allows all people to look like the best version of<br>themselves, not an aspirational version of someone else.";
+
+document.getElementById("x-out").addEventListener("click", (e) => {
+  document.getElementById("banner").remove();
+  e.preventDefault();
+});
