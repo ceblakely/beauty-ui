9897af5c8375539781b8d0850e3ee08de24456cf
ceblakely <cblakelydev@gmail.com>
2021-12-25 21:27:24 -0600

    Updated JS for the reviews section

--- public/home.js
+++ public/home.js
@@ -1,21 +1,48 @@
-function changeQuote(id) {
-  let quoteP = document.getElementById("quoteP");
-  if (id == "vogueImg") {
-    quoteP.innerHTML =
-      "In an age of online access and unending choice, customer engagement is something that Fenty has gotten right.";
-  } else if (id == "elleImg") {
-    quoteP.innerHTML =
-      "Revolutionary for its time, harnessing a certain come-as-you-are acceptance.";
-  } else {
-    quoteP.innerHTML =
-      "Allows all people to look like the best version of<br>themselves, not an aspirational version of someone else.";
-  }
-}
+const reviews = {
+  0: (vogue = {
+    imagePath: "/images/HomeImages/vogue.png",
+    quote:
+      "In an age of online access and unending choice, customer engagement is something that Fenty has gotten right.",
+  }),
+  1: (elle = {
+    imagePath: "/images/HomeImages/elle.png",
+    quote:
+      "Revolutionary for its time, harnessing a certain come-as-you-are acceptance.",
+  }),
+  2: (vanityFair = {
+    imagePath: "/images/HomeImages/vanityfair1.png",
+    quote:
+      "Allows all people to look like the best version of themselves, not an aspirational version of someone else.",
+  }),
+};
+let reviewImg = document.getElementById("reviewImg");
+let currentIndex = 0;
+
+document.querySelectorAll(".arrow").forEach((arrow) => {
+  arrow.addEventListener("click", (e) => {
+    if (arrow.id == "forwardArrow") {
+      currentIndex++;
+      if (currentIndex > 2) {
+        currentIndex = 0;
+      }
+    } else if (arrow.id == "backArrow") {
+      if (currentIndex == 0) {
+        currentIndex = 2;
+      } else {
+        currentIndex--;
+      }
+    }
+    reviewImg.src = reviews[currentIndex].imagePath;
+    document.getElementById("quoteP").innerHTML = reviews[currentIndex].quote;
+    e.preventDefault();
+  });
+});
 document.getElementById("x-out").addEventListener("click", (e) => {
   document.getElementById("banner").remove();
   e.preventDefault();
 });
 window.addEventListener("load", () => {
-  document.getElementById("quoteP").innerHTML =
-    "In an age of online access and unending choice, customer engagement is something that Fenty has gotten right.";
+  reviewImg.src = reviews[0].imagePath;
+  reviewImg.classList.add("activeImg");
+  document.getElementById("quoteP").innerHTML = reviews[0].quote;
 });
